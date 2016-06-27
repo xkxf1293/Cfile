ed2b791095d234e9449c9cf6bf7cff8925a66ab2
Solor Asker <xkxf1293@gmail.com>
2016-06-27 19:07:16 +0800

    Update rename.c

--- rename.c
+++ rename.c
@@ -16,7 +16,7 @@ void changename(char *p)
 	j=0;
 	char tail1[100];
 	strlen(p);
-	printf("P is %d\n",strlen(p));	
+	//printf("P is %d\n",strlen(p));	
 	for (i=strlen(p)-12;i<strlen(p)-4 ; i++)
 	{
 		tail1[j]=p[i];
@@ -24,12 +24,12 @@ void changename(char *p)
 	}
 	p[i-8]='\0';
 	tail1[j]='\0';
-	printf("p1 is %s\n",p);
-	printf("tail1 is %s\n",tail1);
+	//printf("p1 is %s\n",p);
+	//printf("tail1 is %s\n",tail1);
 	strcat(tail1,p);
 	strcpy(p,tail1);
 	strcat(p,".pdf");
-	printf("p1 is %s\n",p);
+	//printf("p1 is %s\n",p);
 }
 
 void SearchFile(char *dir)
@@ -42,25 +42,25 @@ void SearchFile(char *dir)
     sprintf(path,"%s\\*.pdf*",dir); 
     sprintf(path1,"%s\\",dir);  
     long done = _findfirst(path,&ffblk);   
-    printf("done is %d\n",done);
+    //printf("done is %d\n",done);
     int find=0;  
-    printf("%s\n",path);
+    //printf("%s\n",path);
     while (find==0)     
     {     
         // Compare strings
 		if(strcmp(ffblk.name,".pdf"))
         {   
-            printf(" strcmp is %d. \n ffblk.name is %s\n",strcmp(ffblk.name,".pdf"),ffblk.name);
+            //printf(" strcmp is %d. \n ffblk.name is %s\n",strcmp(ffblk.name,".pdf"),ffblk.name);
 			// Copy "path1"  to "path2"
 			strcpy(path2,path1);
-			printf("path2 is %s\n",path2);
+			//printf("path2 is %s\n",path2);
 			// Copy "ffblk.name" string to the end of "path2"
             strcat(path2,ffblk.name);
            // system(path2);
-            printf("Rename Files: %s\n Press 'Y' or 'N'\n",path2);
-            scanf("%c",&ch);
-            if(ch=='Y' || ch=='y')
-            {
+            // printf("Rename Files: %s\n Press 'Y' or 'N'\n",path2);
+            // scanf("%c",&ch);
+            // if(ch=='Y' || ch=='y')
+            // {
                 strcpy(path3,path1);
 				strcpy(name1,ffblk.name);
 				changename(name1);
@@ -71,9 +71,9 @@ void SearchFile(char *dir)
                 else
 					// Print the last system error messages
                    perror( "Rename Error!!!"); 
-            }
+            //}
 			// Reads characters from the console and immediately echo
-            getchar();
+            //getchar();
         }
         find=_findnext(done,&ffblk);
     }
