5e88ea5e028f7b446e2f1a5f4dec49301ae4368d
Solor Asker <xkxf1293@gmail.com>
2016-06-27 17:55:17 +0800

    Update rename.c

--- rename.c
+++ rename.c
@@ -10,14 +10,34 @@
 
 //void SearchFile(char *);   
 
-
+void changename(char *p)
+{
+	int i,j;
+	j=0;
+	char tail1[100];
+	strlen(p);
+	printf("P is %d\n",strlen(p));	
+	for (i=strlen(p)-12;i<strlen(p)-4 ; i++)
+	{
+		tail1[j]=p[i];
+		j++;		
+	}
+	p[i-8]='\0';
+	tail1[j]='\0';
+	printf("p1 is %s\n",p);
+	printf("tail1 is %s\n",tail1);
+	strcat(tail1,p);
+	strcpy(p,tail1);
+	strcat(p,".pdf");
+	printf("p1 is %s\n",p);
+}
 
 void SearchFile(char *dir)
 {       
     // Define structure
     struct _finddata_t   ffblk; 
 	// Define arrays
-    char path[256],path1[256],path2[256];
+    char path[256],path1[256],path2[256],path3[256],name1[256];
     char ch;   
     sprintf(path,"%s\\*.pdf*",dir); 
     sprintf(path1,"%s\\",dir);  
@@ -27,21 +47,32 @@ void SearchFile(char *dir)
     printf("%s\n",path);
     while (find==0)     
     {     
-        if(strcmp(ffblk.name,".pdf"))
+        // Compare strings
+		if(strcmp(ffblk.name,".pdf"))
         {   
-            printf("strcmp is %d\n",strcmp(ffblk.name,".pdf"));
+            printf(" strcmp is %d. \n ffblk.name is %s\n",strcmp(ffblk.name,".pdf"),ffblk.name);
+			// Copy "path1"  to "path2"
 			strcpy(path2,path1);
+			printf("path2 is %s\n",path2);
+			// Copy "ffblk.name" string to the end of "path2"
             strcat(path2,ffblk.name);
            // system(path2);
-            printf("Delete Files: %s\n Press 'Y' or 'N'\n",path2);
+            printf("Rename Files: %s\n Press 'Y' or 'N'\n",path2);
             scanf("%c",&ch);
             if(ch=='Y' || ch=='y')
             {
-                if   (remove(path2)   ==   0) 
-                   printf( "Have Removed:   %s.\n ",path2); 
-                else 
-                   perror( "remove "); 
+                strcpy(path3,path1);
+				strcpy(name1,ffblk.name);
+				changename(name1);
+				strcat(path3,name1);
+				// path2 is the oldname, path is the newname
+				if   (rename(path2,path3)   ==   0) 
+                   printf( "Have renamed:   %s\n ",path3); 
+                else
+					// Print the last system error messages
+                   perror( "Rename Error!!!"); 
             }
+			// Reads characters from the console and immediately echo
             getchar();
         }
         find=_findnext(done,&ffblk);
